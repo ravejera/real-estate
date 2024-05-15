8c1b4c5f3885d8175b8ecf52f71b09b37c60439d
GeraldRavera <raveragerald960@gmail.com>
2024-05-15 16:10:29 +0300

    starter real estate project

---
+++ src/utilis/accordion.jsx
@@ -0,0 +1,24 @@
+import { HiShieldCheck } from "react-icons/hi";
+import { MdAnalytics, MdCancel } from "react-icons/md";
+
+
+
+const data = [
+    {
+        icon: <HiShieldCheck/>,
+        heading: "Best interest rates on the market",
+        details: "create details"
+    },
+    {
+        icon: <MdCancel/>,
+        heading: "prevent unstable prices",
+        details: "create details"
+    },
+    {
+        icon: <MdAnalytics/>,
+        heading: "prevent unstable prices",
+        details: "create details"
+    },
+];
+
+export default data;
